4de5a22213b196a5b59bce6064ab7c951e3a8c72
ho sf <hosf@hos-MacBook-Pro.local>
2020-03-19 18:26:26 +0800

    first commit

---
+++ src/fe/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Cms from './Cms';
+import Login from './Login';
+
+const App = () => (
+  <div className="app-routes">
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={Cms} />
+    </Switch>
+  </div>
+);
+
+export default App;
---
+++ src/fe/components/UserAdd.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { post } from 'axios';
+import UserForm from './UserForm';
+import { Helmet } from 'react-helmet';
+import Page from './Page';
+
+class UserAdd extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  handleSubmit(user) {
+    post('/api/users', user)
+      .then(({ data: u }) => {
+        const { history } = this.props;
+        history.push(`/users/${u.id}`);
+        console.log('added:', user);
+      });
+  }
+
+  handleCancel(e) {
+    e.preventDefault();
+    const { history } = this.props;
+    history.push('/users');
+
+    console.log('you have canceled');
+  }
+
+  render() {
+    return (
+      <Page title="Add User" columns={3}>
+        <Helmet>
+          <title>CMS | Add User</title>
+        </Helmet>
+
+        <UserForm
+          handleSubmit={this.handleSubmit}
+          handleCancel={this.handleCancel}
+        />
+      </Page>
+    );
+  }
+}
+
+export default UserAdd;
